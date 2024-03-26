0112c86b3bdcede0fa109e707d6e7386ae2562bf
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-03-26 15:03:06 +0330

    compeleted about us and contact us pages

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import styles from "./AboutUs.module.css";
+
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <>
+      <Navbar />
+      <div className={styles.aboutus} >
+      <div className={styles.container}>
+        <p>
+          فروشگاه «می ایران» از سال ۱۳۹۶ فعالیت خود را با هدف ایجاد فروشگاهی
+          متفاوت در ارائه محصولات و خدمات شیائومی آغاز کرد و در ادامه با فراگیر
+          شدن کسب و کار مجازی اقدام راه اندازی فروشگاه اینترنتی کرد تا خدمات خود
+          را در سطح کشور گسترش دهد و نیاز مشتریان را در خرید آسان برطرف نماید.
+          <br />
+          این مجموعه با استفاده درست از انتقادات و تجارب طی این چندسال توانسته
+          محیطی امن و با اطمینان برای خرید مشتریان فراهم کند. شعار ما این است:
+          <br />
+          متفاوت بودن گران نیست! با ارائه محصول اصلی و با کیفیت همراه با قیمت
+          مناسب سعی در تحقق این شعار داریم. منتظر نظرات و پیشنهادات شما هستیم.
+          <br />
+          مدیریت فروشگاه می ایران
+
+        </p>
+      </div>
+
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./ContactUs.module.css"
+
+
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 function ContactUs() {
   return (
-    <div>ContactUs</div>
-  )
+    <>
+      <Navbar />
+      <div className={styles.aboutus}>
+        <div className={styles.container}>
+          <p>
+            آدرس: اصفهان، جاده اصفهان شیراز
+            <br />
+            شماره تماس ثابـــت:0000000000
+            <br />
+            شماره تماس همراه: 0000000000
+            <br />
+            ساعات پاسخگویی فروشگاه می ایران، روزهای کاری از ساعت ۱۰ الی ۲۰
+            میباشد.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
