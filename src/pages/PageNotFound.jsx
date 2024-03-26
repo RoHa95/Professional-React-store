8dacc3bd47c408a838192af65df45c2002002208
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-03-26 14:35:25 +0330

    designed page not found component

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import styles from "./PageNotFound.module.css";
 
 function PageNotFound() {
   return (
     <>
-    <Navbar/>
-    <div>PageNotFound</div>
-    <Footer/>
+      <Navbar />
+      <div className={styles.container}>
+        <p className={styles.notfoundTag}>صفحه مورد نظر شما یافت نشد.</p>
+        <img
+          className={styles.imageContainer}
+          src="https://s31.picofile.com/file/8473836826/p.png"
+        ></img>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
